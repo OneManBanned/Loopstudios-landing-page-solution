760b4c10626a84d08bda0bd6937a5626a6df70cd
Brendan Lawless <brendanlawless@hotmail.com>
2023-08-06 11:28:20 +0100

    Fixed layout at larger screen sizes

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const button = document.querySelector('#hamburger')
 const nav = document.querySelector('nav')
 const body = document.querySelector('body')
+const { classList } = nav
 
-button.addEventListener('click', () => {
-    button.classList.toggle('close')
-    nav.classList.toggle('open')
-    body.classList.toggle('fixed')
-})
+button.onclick = () => toggleMenu()
+
+function toggleMenu() {
+    classList.toggle('navOpen')
+    if (classList.contains('navOpen')) {
+        button.style.background = 'center center url(./images/icon-close.svg) no-repeat transparent'
+    } else {
+        button.style.background = 'center center url(./images/icon-hamburger.svg) no-repeat transparent'
+    }
+}
+
+// button.classList.toggle('open')
+// nav.classList.toggle('open')
+// body.classList.toggle('fixed')
